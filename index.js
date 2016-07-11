5eb07eea64bf2a663f53239442b2a35158c0c90e
unknown <>
2016-07-11 18:59:27 +0800

    888

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/willywu', function(request, response) {
+  response.send("102062124");
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
