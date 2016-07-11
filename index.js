ef2fa6ffea9866a840c8056491921b051b553672
unknown <>
2016-07-11 20:11:42 +0800

    333

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: false
 }));
 
 // views is directory for all template files
