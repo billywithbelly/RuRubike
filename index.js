87b5b30a9758cd55330d34cfa8b7e363d549c1a0
unknown <>
2016-07-18 11:17:27 +0800

    2

--- index.js
+++ index.js
@@ -49,13 +49,13 @@ app.get('/searchid', function(request, response) {
 });
 
 function findCourses(request, response){
-	var data = request.query;
+	var data = request.query.course;
 	var answer = {};
 	var courses = db.collection("courses");
-	courses.find({"course" : {"$in":data.course}},{_id:0}).toArray(function(err,callBack) 
+	courses.find({course : {"$in":data}},{_id:0}).toArray(function(err,callBack) 
 	{
 		answer = callBack;
-		response.send(request);
+		response.send(data);
 	});
 }
 
