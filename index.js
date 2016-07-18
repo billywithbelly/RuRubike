610d6aa5a7ad2ebfd20483529e15580e0cff61b3
unknown <>
2016-07-18 11:32:15 +0800

    321

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function findCourses(request, response){
 	courses.find({course : {"$in":data.course}},{_id:0}).toArray(function(err,callBack) 
 	{
 		answer = callBack;
-		response.send(data.course);
+		response.send(answer);
 	});
 }
 
