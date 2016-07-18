65328183ba6ef13f0d5775ba6ad4866dc1daa127
unknown <>
2016-07-18 11:15:38 +0800

    d

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ function findCourses(request, response){
 	var data = request.query;
 	var answer = {};
 	var courses = db.collection("courses");
-	courses.find({"course" : {data.course}},{_id:0}).toArray(function(err,callBack) 
+	courses.find({"course" : {"$in":data.course}},{_id:0}).toArray(function(err,callBack) 
 	{
 		answer = callBack;
-		response.send(answer);
+		response.send(request);
 	});
 }
 
