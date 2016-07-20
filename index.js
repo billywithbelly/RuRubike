a41e33a665459af5f829f5f433f951c20920eb4b
willylulu <maya6282@gmail.com>
2016-07-20 14:51:44 +0800

    156

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/testing');
 });
 
 app.get('/testing', function(request, response) {
