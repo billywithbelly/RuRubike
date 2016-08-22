0c433f9563ad072cd9987157fd72e4d51079f599
willylulu <maya6282@gmail.com>
2016-08-22 12:48:22 +0800

    幹忘了開branch改

--- include/api/account/account.js
+++ include/api/account/account.js
@@ -1,5 +1,4 @@
-var md5 = require('js-md5');
-var htmlspecialchars = require('htmlspecialchars');
+var func = require('../../function/function.js');
 var mongoDataBase;
 
 exports.bindDB = function(db) {
@@ -9,15 +8,15 @@ exports.bindDB = function(db) {
 exports.bindApp = function(app) {
 
 	app.post('/update/password',function(req, res) {
-		var data = antiXSS(req.body);
-		updatePassword(data.id, md5(data.password), md5(data.newpassword), function(response) {
+		var data = func.antiXSS(req.body);
+		updatePassword(data.id, func.md5(data.password), func.md5(data.newpassword), function(response) {
 			res.send(response);
 		});
 	});
 
 	app.post('/update/email',function(req, res) {
-		var data = antiXSS(req.body);
-		updateEmail(data.id, md5(data.password), data.newemail, function(response) {
+		var data = func.antiXSS(req.body);
+		updateEmail(data.id, func.md5(data.password), data.newemail, function(response) {
 			res.send(response);
 		});
 	});
@@ -28,22 +27,22 @@ var updatePassword = function(id, password, newpassword, callback) {
 	mongoDataBase.getAccount({ id : id }, function(err, res) {
 
 		if(err) 
-			callback(dberror());
+			callback(func.dberror());
 		else {
 			if(res.length === 0) {
-				callback(result("no account", -1));
+				callback(func.result("no account", -1));
 			}
 			else {
 				if(res[0].password !== password) {
-					callback(result("wrong password", -1));
+					callback(func.result("wrong password", -1));
 				}
 				else {
 					mongoDataBase.updateAccountPassword(id, newpassword, function(err, res) {
 						if(err) {
-							callback(dberror());
+							callback(func.dberror());
 						}
 						else {
-							callback(result("update password success", 1));
+							callback(func.result("update password success", 1));
 						}
 					});
 				}
@@ -57,48 +56,23 @@ var updateEmail = function(id, password, newemail, callback) {
 	mongoDataBase.getAccount({ id : id }, function(err, res) {
 
 		if(err) 
-			callback(dberror());
+			callback(func.dberror());
 		else {
 			if(res.length === 0) {
-				callback(result("no account", -1));
+				callback(func.result("no account", -1));
 			}
 			else {
 				if(res[0].password !== password) {
-					callback(result("wrong password", -1));
+					callback(func.result("wrong password", -1));
 				}
 				mongoDataBase.updateAccountEmail(id, newemail, function(err, res) {
 					if(err) 
-						callback(dberror());
+						callback(func.dberror());
 					else {
-						callback(result("update email success", 1));
+						callback(func.result("update email success", 1));
 					}
 				});
 			}
 		}
 	});
-}
-
-var dberror = function() {
-	var temp = {};
-	temp.result = "db error";
-	temp.code = -2;
-	return temp;
-}
-
-var result = function (result,code) {
-	// body...
-	var temp = {};
-	temp.result = result;
-	temp.code = code;
-	return temp;
-}
-
-var antiXSS =  function(data) {
-    var ans = data;
-    for(var key in ans){
-        if(typeof(ans[key])=="string"){
-            ans[key] = htmlspecialchars(ans[key]);
-        }
-    }
-    return ans;
 }
