36df1ef13b7d94c516eb63c533a0d098f7303004
willylulu <maya6282@gmail.com>
2016-12-09 11:06:26 +0800

    651

--- public/js/face.js
+++ public/js/face.js
@@ -3,24 +3,30 @@ $(document).ready(function(){
 });
 
 function videoInit() {
-    navigator.getUserMedia = navigator.getUserMedia ||
-                           navigator.webkitGetUserMedia ||
-                           navigator.mozGetUserMedia;
 
-    if (navigator.getUserMedia) {
-     navigator.getUserMedia({ audio: false, video: true },
-        function(stream) {
-           var video = document.querySelector('video');
-           video.src = window.URL.createObjectURL(stream);
-           video.onloadedmetadata = function(e) {
-             video.play();
-           };
-        },
-        function(err) {
-           console.log("The following error occurred: " + err.name);
-        }
-     );
-    } else {
-     console.log("getUserMedia not supported");
-    }
+  navigator.getUserMedia = navigator.getUserMedia ||  navigator.webkitGetUserMedia ||  navigator.mozGetUserMedia;
+
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia(
+      { audio: false, video: true },
+      function(stream) {
+        var video = document.querySelector('video');
+        video.src = window.URL.createObjectURL(stream);
+        video.onloadedmetadata = function(e) {
+          video.play();
+        };
+        canvasFeetVideo();
+      },
+      function(err) {
+        console.log("The following error occurred: " + err.name);
+      }
+    );
+  } 
+  else {
+    console.log("getUserMedia not supported");
   }
+}
+
+function canvasFeetVideo(){
+  $('#screenShotVideo').width($('#faceDetectVideo').width());
+}
