01e3ec02256451ff743a4c0ecb82a5196e613837
willylulu <maya6282@gmail.com>
2016-12-09 11:50:32 +0800

    1165

--- public/js/face.js
+++ public/js/face.js
@@ -3,20 +3,31 @@ $(document).ready(function(){
     videoInit();
 });
 
+var video;
+var canvas;
+var height;
+var streaming;
+
 function videoInit() {
 
   navigator.getUserMedia = navigator.getUserMedia ||  navigator.webkitGetUserMedia ||  navigator.mozGetUserMedia;
 
   if (navigator.getUserMedia) {
-    var rect = document.getElementById('faceDetectVideo').getBoundingClientRect();
     navigator.getUserMedia(
-      { audio: false, video: { width: rect.width, height: rect.height } },
+      { audio: false, video: true },
       function(stream) {
-        var video = document.querySelector('video');
+        video = document.getElementById('video');
+        canvas = document.getElementById('canvas');
         video.src = window.URL.createObjectURL(stream);
         video.onloadedmetadata = function(e) {
           video.play();
         };
+        video.addEventListener('canplay', function(ev){
+          video.setAttribute('width', video.videoWidth);
+          video.setAttribute('height', video.videoheight);
+          canvas.setAttribute('width', video.videoWidth);
+          canvas.setAttribute('height', video.videoheight);
+        }, false);
       },
       function(err) {
         console.log("The following error occurred: " + err.name);
@@ -29,10 +40,8 @@ function videoInit() {
 }
 
 function screenShotButHendler(){
-  var canvas = document.getElementById('screenShotVideo');
-  var ctx = canvas.getContext('2d');
-  var rect = document.getElementById('screenShotVideo').getBoundingClientRect();
-  var video = document.querySelector('video');
-  console.log(rect);
-  ctx.drawImage(video, 0, 0);
+  var context = canvas.getContext('2d');
+  canvas.width = width;
+  canvas.height = height;
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
 }
