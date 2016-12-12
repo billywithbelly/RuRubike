4d77feb104ebd95e17612d01b14c011982b38951
willylulu <maya6282@gmail.com>
2016-12-13 01:08:23 +0800

    12465

--- public/js/face.js
+++ public/js/face.js
@@ -251,7 +251,7 @@ function OutputConclusion(persistedFaceId){
   var url = data.url;
   var img = document.createElement('img');
   img.src = url;
-  img.width = $("#OutputFaceImg")[0].getBoundingClientRect().width;
+  img.width = $("#OutputModal")[0].getBoundingClientRect().width/3;
   img.onload = function(){
     $("#OutputFaceImg").html("");
     $("#OutputFaceImg").append(img);
@@ -260,7 +260,7 @@ function OutputConclusion(persistedFaceId){
   var inputUrl = canvas.toDataURL();
   var inputImg = document.createElement('img');
   inputImg.src = inputUrl;
-  inputImg.width = $("#InputFaceImg")[0].getBoundingClientRect().width;
+  inputImg.width = $("#OutputModal")[0].getBoundingClientRect().width/3;
   inputImg.onload = function(){
     $("#InputFaceImg").html("");
     $("#InputFaceImg").append(inputImg);
