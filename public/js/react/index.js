2c928fe4d393b1fa9f3ba59011c0b767957db80d
Safanar <satan251008@gmail.com>
2017-04-28 01:10:48 +0800

    Update index.js

--- public/js/react/index.js
+++ public/js/react/index.js
@@ -7,7 +7,7 @@ import UI from '../components/UI'
 var ui;
 var map;
 var socket;
-
+/*
 function initialize() {
 	map = new Map('googleMapDiv');
 	socket = new Socket(map);
@@ -23,4 +23,4 @@ google.maps.event.addDomListener(
   window, 'load', initialize);
 
 export {map,ui};
-
+*/
